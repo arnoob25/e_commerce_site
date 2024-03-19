7e239f3fb1065266c511c82d6d72f814dd67e3e3
Arnob <arnob@gmail.com>
2024-03-19 15:11:20 +0600

    Minor changes

--- transactions/models.py
+++ transactions/models.py
@@ -8,9 +8,9 @@ user_model = get_user_model()
 
 class Order(models.Model):
     """manage buying requests, carts, wishlist, etc."""
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    buyer = models.ForeignKey(user_model, on_delete=models.CASCADE)
-    seller = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    buyer = models.ForeignKey(user_model, on_delete=models.SET_NULL)
+    seller = models.ForeignKey(user_model, on_delete=models.SET_NULL)
     quantity = models.IntegerField()
     is_promo_applied = models.BooleanField(default=False)
     is_confirmed = models.BooleanField(default=False)
@@ -18,7 +18,7 @@ class Order(models.Model):
 
 class Transaction(models.Model):
     """track deliveries, store transaction records, etc."""
-    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    orders = models.ManyToManyField(Order)
     buyer = models.ForeignKey(user_model, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     total_amount = models.FloatField()
